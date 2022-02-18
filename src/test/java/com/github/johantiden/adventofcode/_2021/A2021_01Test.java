d2e69f7d78344e08545a36d060c4afb6c0f2da9a
Johan Tidén <johan.tiden@looklet.com>
2022-02-18 18:40:36 +0100

    To test class

--- src/test/java/com/github/johantiden/adventofcode/_2021/A2021_01Test.java
+++ src/test/java/com/github/johantiden/adventofcode/_2021/A2021_01Test.java
@@ -2,12 +2,17 @@ package com.github.johantiden.adventofcode._2021;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class A2021_01Test {
 
     @Test
-    public void name() {
+    public void testResults() {
+        assertThat(A2021_01.a(A2021_01.EXAMPLE), is(7));
+        assertThat(A2021_01.a(A2021_01.REAL), is(1387));
 
+        assertThat(A2021_01.b(A2021_01.EXAMPLE), is(5));
+        assertThat(A2021_01.b(A2021_01.REAL), is(1362));
     }
 }
