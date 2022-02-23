7ef5c550319683b5fc0dc94fdf6110e927ee3e6a
Johan Tidén <johan.tiden@looklet.com>
2022-02-23 10:20:57 +0100

    Dump of failed b. I didnt read the instructions

--- src/main/java/com/github/johantiden/adventofcode/_2021/A2021_12.java
+++ src/main/java/com/github/johantiden/adventofcode/_2021/A2021_12.java
@@ -5,6 +5,9 @@ import com.github.johantiden.adventofcode.common.JList;
 import com.github.johantiden.adventofcode.common.graph.Edge;
 import com.github.johantiden.adventofcode.common.graph.Graph;
 
+import java.util.Comparator;
+import java.util.function.BiFunction;
+
 public class A2021_12 {
 
     static final String EXAMPLE =
@@ -19,35 +22,60 @@ public class A2021_12 {
         Graph graph = parse(input);
         WalkState state = new WalkState(graph, JList.of("start"));
 
-        JList<JList<String>> possibleWalks = state.getPossibleWalks();
+        JList<JList<String>> possibleWalks = state.getPossibleWalks(WalkState::pruneLowercaseWhenLeavingIt)
+                .distinct();
 
         return possibleWalks.size();
     }
 
     static long b(String input) {
-        return -1;
+        Graph graph = parse(input);
+        WalkState state = new WalkState(graph, JList.of("start"));
+
+        JList<JList<String>> possibleWalks = state.getPossibleWalks(WalkState::pruneLowercaseWhenLeavingItAndHasBeenThereTwice)
+                .distinct();
+
+        return possibleWalks.size();
+    }
+
+    static String toString(JList<JList<String>> possibleWalks) {
+        return possibleWalks
+                .map(l -> l.toString().replaceAll(" ", ""))
+                .sorted(Comparator.comparing(s -> s.replaceAll(",", "")))
+                .toString();
     }
 
     record WalkState(Graph graph, JList<String> pathSoFar) {
-        public JList<JList<String>> getPossibleWalks() {
+        public JList<JList<String>> getPossibleWalks(BiFunction<WalkState, String, Graph> pruner) {
             String currentPosition = pathSoFar.last();
             if (currentPosition.equals("end")) {
                 return JList.of(pathSoFar);
             } else {
-                JList<String> possibleSteps = graph.getPossibleStepsFrom(currentPosition);
+                JList<String> possibleSteps = graph.getPossibleStepsFrom(currentPosition)
+                        .sorted(Comparator.naturalOrder()); // Sorting not necessary but produces the same output as the examples.
                 return possibleSteps
                         .flatMap(step -> {
-                            WalkState stateAfterStep = new WalkState(pruneLowercaseWhenLeavingIt(graph, currentPosition), pathSoFar.plus(step));
-                            return stateAfterStep.getPossibleWalks();
+                            WalkState stateAfterStep = new WalkState(pruner.apply(this, currentPosition), pathSoFar.plus(step));
+                            return stateAfterStep.getPossibleWalks(pruner);
                         });
             }
         }
 
-        private static Graph pruneLowercaseWhenLeavingIt(Graph graph, String step) {
-            if (isLowercase(step)) {
-                return graph.withoutNode(step);
+        private static Graph pruneLowercaseWhenLeavingIt(WalkState walkState, String leaving) {
+            if (isLowercase(leaving)) {
+                return walkState.graph.withoutNode(leaving);
             } else {
-                return graph;
+                return walkState.graph;
+            }
+        }
+
+        private static Graph pruneLowercaseWhenLeavingItAndHasBeenThereTwice(WalkState walkState, String leaving) {
+            boolean hasBeenThereTwice = walkState.pathSoFar.filter(node -> node.equals(leaving)).size() == 2;
+            boolean allowedToGoBackAgain = !(isLowercase(leaving) && hasBeenThereTwice) && !leaving.equals("start") && !leaving.equals("end");
+            if (!allowedToGoBackAgain) {
+                return walkState.graph.withoutNode(leaving);
+            } else {
+                return walkState.graph;
             }
         }
 
